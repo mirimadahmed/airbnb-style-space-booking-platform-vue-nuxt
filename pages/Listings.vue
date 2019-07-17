fdb10ade36b5520f84279ee34980373539f80e86
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-07-17 12:02:25 +0500

    listings here

--- pages/Listings.vue
+++ pages/Listings.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Listings here</div>
+  <div class="m-5 p-4">
+    <div class="row">
+      <h3>My Listings</h3>
+      <div class="col-md-12 bg-light">
+        <div class="row" v-if="listings.length === 0">
+          <div class="col-md-12 my-2">Create a listing now.</div>
+        </div>
+        <div class="row" v-for="(listing, i) in listings" :key="i">
+          <div class="col-md-12 my-2">Listing 1</div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["listings"])
+  }
+};
 </script>
 
 <style lang="scss" scoped>
