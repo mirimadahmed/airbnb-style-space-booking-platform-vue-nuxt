075fc7a62db71da8b0d7293a00f91cd805dd4a6e
Rabbit <dawoodahmed696@gmail.com>
2019-07-29 12:09:44 +0500

    vue google maps

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -10,8 +10,10 @@ import { setContext, getLocation, getRouteData, normalizeError } from './utils'
 import { createStore } from './store.js'
 
 /* Plugins */
+
 import nuxt_plugin_bootstrapvue_077ab82a from 'nuxt_plugin_bootstrapvue_077ab82a' // Source: .\\bootstrap-vue.js (mode: 'all')
 import nuxt_plugin_fontawesome_b8db358e from 'nuxt_plugin_fontawesome_b8db358e' // Source: ..\\plugins\\fontawesome.js (mode: 'all')
+import nuxt_plugin_vue2googlemaps_51da65b7 from 'nuxt_plugin_vue2googlemaps_51da65b7' // Source: ..\\plugins\\vue2-google-maps.js (mode: 'client')
 import nuxt_plugin_vuemultiselect_7aa8df7f from 'nuxt_plugin_vuemultiselect_7aa8df7f' // Source: ..\\plugins\\vue-multiselect.js (mode: 'all')
 import nuxt_plugin_antd_975cdd78 from 'nuxt_plugin_antd_975cdd78' // Source: ..\\plugins\\antd.js (mode: 'all')
 import nuxt_plugin_datepicker_4a26849c from 'nuxt_plugin_datepicker_4a26849c' // Source: ..\\plugins\\datepicker (mode: 'client')
