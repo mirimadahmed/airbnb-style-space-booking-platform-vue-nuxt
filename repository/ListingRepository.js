99f430129c0f564143626c42aeb8963ef9de4c36
Rabbit <dawoodahmed696@gmail.com>
2019-07-29 18:01:42 +0500

    getting entity slots based on date SOLVED  modifying slots SOLVED

--- repository/ListingRepository.js
+++ repository/ListingRepository.js
@@ -8,8 +8,16 @@ export default {
   getAll(company_id) {
     return Repository.get(`${resource}company/${company_id}`)
   },
-  getEntitySlots(company_id) {
-    return Repository.get(`entity_slots/${company_id}`)
+  getEntitySlots(payload) {
+    return Repository.get(`entity_slots/${payload.entity_id}?dnr_rate=`+payload.selectedDate)
+  },
+  modifyEntitySlots(payload) {
+    if(payload.dnr){
+      return Repository.post(`entity_dnr/${payload.entity_id}/${payload.slot_id}?dnr_date=`+payload.selectedDate)
+    }
+    else{
+      return Repository.delete(`delete_entity_dnr/${payload.dnr_id}`)
+    }
   },
   newListing(company_id, type, title, description, address, lat, lng) {
     return Repository.post(`${resource}`, {
