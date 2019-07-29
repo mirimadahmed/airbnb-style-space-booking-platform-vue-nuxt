65bfb1d1526170b373d0ce085b66f71a10341faa
Rabbit <dawoodahmed696@gmail.com>
2019-07-29 16:17:46 +0500

    handling how calendar components are loaded and

--- pages/Calander.vue
+++ pages/Calander.vue
@@ -8,7 +8,10 @@
         <div class="row">
           <div class="col-md-6">
             <div class="row">
-              <div class="col-md-12">
+              <div class="col-md-12" v-if="isLoading && spaces.length==0">
+                <b-spinner variant="danger" type="grow" label="Spinning"></b-spinner>
+              </div>
+              <div class="col-md-12" v-else>
                 <b-form-select v-model="selectedSpace" :disabled="isLoading" :options="spaceOptions"></b-form-select>
               </div>
               <div v-if="selectedSpace!=null" class="col-md-12 mt-5">
@@ -18,7 +21,7 @@
               </div>
             </div>
           </div>
-          <div v-if="slotsGenerated.length>0 && isLoading==false" class="col-md-6 px-5">
+          <div v-if="slotsGenerated.length>0" class="col-md-6 px-5">
             <div class="row header">
               <div class="col-md-6 text-left p-2">Timing</div>
               <div class="col-md-6 text-right p-2">Status</div>
@@ -29,6 +32,7 @@
               >From {{ timeSlot.slot_start }} to {{ timeSlot.slot_end }}</div>
               <div class="col-md-6 p-2 text-right">
                 <b-form-checkbox
+                  @change="toggleIndeterminate()"
                   v-model="timeSlot.dnr"
                   name="check-button"
                   class="slot-switch"
@@ -69,10 +73,13 @@ export default {
     };
   },
   methods: {
+    toggleIndeterminate () {
+      console.log("swiss")
+    },
     async dateSelected(date) {
       this.selectedDate=date;
       this.isLoading = true;
-      const { data } = await ListingRepository.getEntitySlots(this.selectedSpace);
+      const { data } = await ListingRepository.getEntitySlots({company_id:this.selectedSpace,selectedDate:this.selectedDate});
       this.slotsGenerated=data
       this.isLoading = false;
     },
