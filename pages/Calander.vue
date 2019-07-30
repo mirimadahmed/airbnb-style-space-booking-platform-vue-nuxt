fd179cbf7fd29469a5a0cc1b6c4208f7712029d0
Rabbit <dawoodahmed696@gmail.com>
2019-07-30 12:44:55 +0500

    fetching modifying and emptying slots on reselect

--- pages/Calander.vue
+++ pages/Calander.vue
@@ -11,8 +11,8 @@
               <div class="col-md-12" v-if="isLoading && spaces.length==0">
                 <b-spinner variant="danger" type="grow" label="Spinning"></b-spinner>
               </div>
-              <div class="col-md-12" v-else>
-                <b-form-select v-model="selectedSpace" :disabled="isLoading" :options="spaceOptions"></b-form-select>
+              <div class="col-md-12" v-else-if="spaces.length>0">
+                <b-form-select v-model="selectedSpace" :disabled="isLoading" @change="emptySlots()" :options="spaceOptions"></b-form-select>
               </div>
               <div v-if="selectedSpace!=null" class="col-md-12 mt-5">
                 <no-ssr>
@@ -21,7 +21,7 @@
               </div>
             </div>
           </div>
-          <div v-if="slotsGenerated.length>0" class="col-md-6 px-5">
+          <div v-if="slotsGenerated.length>0 && isLoading==false" class="col-md-6 px-5">
             <div class="row header">
               <div class="col-md-6 text-left p-2">Timing</div>
               <div class="col-md-6 text-right p-2">Status</div>
@@ -30,7 +30,7 @@
               <div
                 class="col-md-6 slot-time p-2 text-left"
               >From {{ timeSlot.slot_start }} to {{ timeSlot.slot_end }}</div>
-              <div class="col-md-6 p-2 text-right">
+              <div v-if="!isLoading" class="col-md-6 p-2 text-right">
                 <b-form-checkbox
                   @change="modifySlots(timeSlot.slot_id,!timeSlot.dnr,timeSlot.dnr_id)"
                   v-model="timeSlot.dnr"
@@ -39,11 +39,13 @@
                   switch
                 />
               </div>
-               <!-- :disabled="timeSlot.disabled" -->
-                 
               <hr class="col-md-12" />
             </div>
           </div>
+          <div v-else-if="isLoading || slotsGenerated.length==0 && selectedDate!=null" class="col-md-6 p-2 text-right">
+                <b-spinner variant="danger" type="grow" label="Spinning"></b-spinner>
+          </div>
+                 
         </div>
       </div>
     </div>
@@ -74,16 +76,31 @@ export default {
     };
   },
   methods: {
+    emptySlots(){
+     this.slotsGenerated=[]
+     this.selectedDate=null
+
+    },
     async modifySlots (slot_id,dnr,dnr_id) {
+      this.isLoading=true
       if(dnr) {
       const { data } = await ListingRepository.modifyEntitySlots({dnr:dnr,entity_id:this.selectedSpace,slot_id:slot_id,selectedDate:this.selectedDate});
+      console.log(data)
+      this.fetchSlots()
       }
       else {
-      const { data } = await ListingRepository.modifyEntitySlots({dnr_id:dnr_id,dnr:dnr,entity_id:this.selectedSpace,slot_id:slot_id,selectedDate:this.selectedDate});
+      const { data } = await ListingRepository.modifyEntitySlots({dnr_id:dnr_id});
+      this.fetchSlots()
+
       }
+      this.isLoading=false
     },
-    async dateSelected(date) {
+    dateSelected(date) {
       this.selectedDate=moment(date).format("YYYY/MM/DD");
+      this.fetchSlots()
+ 
+    },
+    async fetchSlots() {
       this.isLoading = true;
       const { data } = await ListingRepository.getEntitySlots({entity_id:this.selectedSpace,selectedDate:this.selectedDate});
       this.slotsGenerated=data
