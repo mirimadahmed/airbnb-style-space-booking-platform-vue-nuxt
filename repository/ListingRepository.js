1b0d40fcfc036d5df9cc126a777cb0e153ea9f6c
Rabbit <dawoodahmed696@gmail.com>
2019-08-01 11:33:12 +0500

    upload gallery images connection

--- repository/ListingRepository.js
+++ repository/ListingRepository.js
@@ -8,6 +8,15 @@ export default {
   getAll(company_id) {
     return Repository.get(`${resource}company/${company_id}`)
   },
+  uploadEntityGalleryImages(payload) {
+    const form = new FormData();
+    payload.files.map(i=>{
+      form.append('files', i); 
+    })
+    form.append('entity_id', payload.entity_id);
+    form.append('file_type', payload.file_type);
+    return Repository.post('file_upload/', form);
+  },
   newListing(new_entity) {
     return Repository.post(`${resource}`, new_entity)
   },
