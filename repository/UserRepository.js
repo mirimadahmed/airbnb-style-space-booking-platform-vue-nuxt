89a8d95324fdef60ca9078608e3d780a99fb7eec
Rabbit <dawoodahmed696@gmail.com>
2019-08-27 13:22:16 +0500

    getting and updating company profile

--- repository/UserRepository.js
+++ repository/UserRepository.js
@@ -1,4 +1,5 @@
 import Repository from './Repository';
+const company_profile_resource = 'companies/';
 
 export default {
   login(request) {
@@ -6,5 +7,15 @@ export default {
   },
   register(request) {
     return Repository.post('/company_signup/', request)
-  }
+  },
+  getCompanyProfile (permalink) {
+    return Repository.get(`${company_profile_resource}`+permalink);
+  },
+  updateVendorProfile (payload) {
+    const form = new FormData()
+    form.append('logo_image', payload.logo_image)
+    form.append('address', payload.address)
+    form.append('phone_no', payload.phone_no)
+      return Repository.put(`${company_profile_resource}`+payload.permalink, form);
+    }
 };
