a88fa71eb72c3c767b85d81a15bfa9797ec1f086
Rabbit <dawoodahmed696@gmail.com>
2019-09-19 11:13:06 +0500

    registration confirmation component

---
+++ pages/RegistrationConfirmation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>You have succesfully created your account</h3>
+  </div>
+</template>
+<script>
+export default {
+  
+  mounted() {
+    console.log(this.$route.query.hash);
+  }
+  
+};
+</script>
+
