37051db83de4194f2dae349369c06baa1cdc278b
Rabbit <dawoodahmed696@gmail.com>
2019-10-01 14:06:38 +0500

    changed footer css

--- components/Footer.vue
+++ components/Footer.vue
@@ -33,7 +33,13 @@
           target="_blank"
           style="color:rgba(0, 0, 0, 0.85);"
           href="http://www.serics.com"
-        >Powered by Serics Global</a>
+        >
+      Powered by Serics Global</a>
+      </div>
+    </div>
+    <div class="row" style="text-align:center;">
+      <div class="col-md-12">
+       <p> Designed & Developed with  <i class="fa fa-heart" aria-hidden="true"></i>  by Serics Technologies </p>
       </div>
     </div>
   </div>
@@ -63,11 +69,14 @@ export default {
 .wrapper {
   color: white;
   padding: 10px;
-  margin: 0px !important;
-  background: #343840;
-  border-top: 0.5px solid rgba(189, 189, 189, 0.637);
-  margin-top: 50px;
-  padding-top: 30px;
+    background: #343840;
+    margin-left: 0px !important;
+    margin-right: 0px !important;
+    background: #343840;
+    border-top: 0.5px solid rgba(189, 189, 189, 0.637);
+    margin-top: 200px;
+    /* margin-bottom: 0px; */
+    padding-top: 30px;
 }
 .contact,
 .copy,
