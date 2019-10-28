4b3ab6e5f61dbb91a9275f6a5bc35f2728a7a15a
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-10-28 22:30:35 +0500

    fine tuned the design

--- pages/Partners.vue
+++ pages/Partners.vue
@@ -7,8 +7,8 @@
       <div class="col-md-12 mb-5">
         <p>We take emmense pride in partnering with these prestegious organizations.</p>
       </div>
-      <div class="col-md-6 text-center" v-for="partner in partners" :key="partner">
-        <img :src="partner" alt="partner logo here" />
+      <div class="col-md-6 p-4 text-center" v-for="partner in partners" :key="partner">
+        <img :src="partner" class="w-50" alt="partner logo here" />
       </div>
     </div>
   </div>
@@ -20,7 +20,9 @@ export default {
     return {
       partners: [
         "https://nicpakistan.pk/wp-content/uploads/2019/04/Artboard-1.png",
-        "https://nicpakistan.pk/wp-content/uploads/2019/02/tup_logo.png"
+        "https://nicpakistan.pk/wp-content/uploads/2019/02/tup_logo.png",
+        "https://knct.pk/wp-content/uploads/2019/08/KNCT-COLOR-copy-_-Final.png",
+        "https://spacesly.s3.amazonaws.com/image.png"
       ]
     };
   }
