4aecbb8603786b5730c746eea0194789927cbc3b
Mir Imad Ahmed <mirimadahmed@outlook.com>
2019-10-28 22:29:35 +0500

    default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
   */
 
   head: {
-    title: 'Spacesly - Book spaces easily.',
+    title: 'Spacesly - Book Event Spaces Easily',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
